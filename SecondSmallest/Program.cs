4432420aedc448d318997cfab1e5d84c83ba53b4
acer <acer@ASUR>
2023-08-13 21:51:31 +0530

    Add project files.

---
+++ SecondSmallest/Program.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SecondSmallest
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            int[] numbers = { 10, 25, 5, 30, 15 };
+            Array.Sort(numbers);
+
+            int secondSmallest = numbers[1];
+
+            Console.WriteLine("Second smallest element: " + secondSmallest);
+            Console.ReadLine();
+        }
+    }
+}
